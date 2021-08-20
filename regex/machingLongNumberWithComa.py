5dbc7fe124fab397e24a1e8114b2a9d3ba18776a
Paweł Łapacz <apach00@wp.pl>
2021-08-20 11:19:09 +0200

    fixing syntax error TODO: corect regexpression

--- regex/machingLongNumberWithComa.py
+++ regex/machingLongNumberWithComa.py
@@ -1,36 +1,39 @@
 #
 # date: 2021-08-20, autor: plapacz6@gmail.com
 
-import regex
+import re
 testSet = [
   ['12', 1],
   ['446', 1],
-  ['1,454' 1],
-  ['34,989'' 1],
+  ['1,454',  1],
+  ['34,989', 1],
   ['994,431', 1],
   ['4,233,210',1],
   ['99,234,219', 1],
   ['434,200,871', 1],
-  ['4,991,090,234', 1]
+  ['4,991,090,234', 1],
   ['452434', 0],
-  [56,8824', 0],
+  ['56,8824', 0],
   ['98,44,799', 0]
 ]
 
 def isNumberWithComa(n):
-  reNumWithComa = r'('d{3},)*(\d{0,3}?)'
-  objReNWC = re(reNumWithComa)
-  if len(objReNWC.group() > 0):
+  #TODO: incorrect regexpression  
+  reNumWithComa = r'(\d{0,3},)*(\d{3},)*(\d{0,3}?)'
+  objReNWC = re.compile(reNumWithComa)
+  mo = objReNWC.search(n)
+  if len(mo.group()) > 0:
     return True
   return False
 
 numberOfTest = 0
 numberOfGoodTest = 0   
-for r,b in testSet:
+numberOfWrongTest = 0
+for t,b in testSet:
   numberOfTest += 1
-  if b == and isNumberWithComa(t):    
+  if b == isNumberWithComa(t):    
     numberOfGoodTest +=1    
   else:
-    print('Test Error: %s, %s', r, b)
-print('Good Test: %s, \nWrong Test: %s\n', numberOfGoodTest, numberOfTest-numberOfGoodTest)
- 
+    print('Test Error:', t,  b)    
+numberOfWrongTest = numberOfTest - numberOfGoodTest
+print('Good Test:', numberOfGoodTest, '\nWrong Test:',  numberOfWrongTest)
